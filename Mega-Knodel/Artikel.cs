bfeae6658df5ee4eec68ee36c0c9955676064f13
Chivas1976 <dovla7608@gmail.com>
2025-05-12 10:29:27 +0200

    hinzufügen Klasse Artikel

---
+++ Mega-Knodel/Artikel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega_Knodel
+{
+    internal class Artikel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Preis { get; set; }
+
+        public Artikel(int id, string name, decimal preis)
+        {
+            Id = id;
+            Name = name;
+            Preis = preis;
+        }
+
+        public void ZeigeInfo()
+        {
+            Console.WriteLine($"[{Id}] {Name} – {Preis} Euro");
+        }
+    }
+}
