dc70c81011acafc57da2ab7e7315b07397ff3460
Chivas1976 <dovla7608@gmail.com>
2025-05-14 12:25:05 +0200

    hinzufügen Kunde.cs

---
+++ Mega-Knodel/Kunde.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega_Knodel
+{
+    internal class Kunde
+    {
+        public void SpeisekarteAnzeigen()
+        {
+            Menue menü = new Menue();
+           // menü.ZeigeMenü();
+        }
+
+        public void BestellungAufgeben(Bestellung bestellung)
+        {
+            bestellung.HauptMenu();
+        }
+    }
+}
