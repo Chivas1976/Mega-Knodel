5bc3bc916bd81ad6b18f5b4ecc6113f03f2c2a2e
Chivas1976 <dovla7608@gmail.com>
2025-05-14 12:23:06 +0200

    Hinzufügen Getraenk.cs

---
+++ Mega-Knodel/Getraenk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega_Knodel
+{
+    internal class Getraenk : Artikel
+    {
+            public Getraenk(int id, string name, decimal preis) : base(id, name, preis)
+            {
+
+            }
+    }
+}
