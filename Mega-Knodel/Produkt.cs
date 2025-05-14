4edb75f69b9dc19ae0bc54b8a52c75d9472e15e8
Chivas1976 <dovla7608@gmail.com>
2025-05-14 12:26:45 +0200

    hinzufügen Produkt.cs

---
+++ Mega-Knodel/Produkt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega_Knodel
+{
+    internal class Produkt:Artikel
+    {
+       
+
+        public Produkt(int id, string name, decimal preis) : base(id, name, preis)
+        {
+            
+        }
+
+       
+       
+    }
+}
