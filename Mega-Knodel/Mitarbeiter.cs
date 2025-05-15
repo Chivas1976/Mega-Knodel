f608dbe55908c2eb5db4583b4d8e7ef8e2e59416
Chivas1976 <dovla7608@gmail.com>
2025-05-15 09:21:52 +0200

    hinzufügen Mitarbeiter.cs

---
+++ Mega-Knodel/Mitarbeiter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega_Knodel
+{
+    internal class Mitarbeiter : Personal
+    {
+
+        public Mitarbeiter(string name, string login, string passwort)
+        {
+            Name = name;
+            Login = login;
+            Passwort = passwort;
+        }
+
+        public void ZeigeInfo()
+        {
+            Console.Clear();
+            Console.WriteLine($"Willkommen {Name}");
+
+        }
+    }
+}
