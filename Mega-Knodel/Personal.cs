515f8dcec7694885134da60434ea188d195c1c81
Chivas1976 <dovla7608@gmail.com>
2025-05-15 09:25:45 +0200

    hinzufügen Personal.cs

---
+++ Mega-Knodel/Personal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega_Knodel
+{
+    class Personal
+    {
+        public string Name { get; set; }
+
+        public string Login { get; set; }
+
+        public string Passwort { get; set; }
+    }
+}
